23048b65fc77140304888018776568153848482d
pbokolo <dev.pbokolo@gmail.com>
2024-06-14 11:58:16 +0200

    Passing the totalPages to the page component

--- app/dashboard/invoices/page.js
+++ app/dashboard/invoices/page.js
@@ -24,7 +24,7 @@ export default async function Page({ searchParams }) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
